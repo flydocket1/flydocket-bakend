57927ed65d4a0b6d0e20c892c7b0902f019fba35
flydocket1 <flydocket1@gmail.com>
2025-04-05 00:07:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import datetime
-
-from scraper import scrape_sample_flights
+from scraper import scrape_sample_flights  # this MUST be present
 
 app = Flask(__name__)
 CORS(app)
